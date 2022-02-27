0e7b4fb63ba80d2f1a551ab3529a9efad5b1d0e7
Mayur Pandey <mayur.pandey@kroo.com>
2022-02-27 12:32:56 +0530

    first commit

---
+++ constants.js
@@ -0,0 +1,103 @@
+const prompts = [
+  ["hi", "hey", "hello", "good morning", "good afternoon"],
+  ["how are you", "how is life", "how are things"],
+  ["what are you doing", "what is going on", "what is up"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you"],
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what call yourself"
+  ],
+  ["who is divyansh pant"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+  ["bad", "bored", "tired"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke"],
+  ["who is prashant chand"],
+  ["who is the hod of second year"],
+  ["who is the hod of first year"],
+  ["who is chairman of graphic era"],
+  ["ok thankyou for your support"],
+  ["how many section are their in second year odd sem"],
+  ["how many student are their in section a"],
+  ["how many student are their in section b"],
+  ["how many student are their in section c"],
+  ["who is classcordinator of section a"],
+  ["who is classcordinator of section b"],
+  ["who is classcordinator of section c"],
+  ["how many company visited this year"],
+  ["who is roll no 1"],
+  ["who is sahil negi"],
+]
+
+
+
+const replies = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],
+  ["I am infinite"],
+  ["I am just a bot", "I am a bot. What are you?"],
+  ["Divyansh created me"],
+  ["I am GEHUBOT", "My creator named me"],
+  ["Divyansh Pant is a student of Graphic Era Hill University, Sem 4,Student Id 20021224"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why? You shouldn't!", "Try watching TV"],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"],
+  ["Prashant chand is the student of GEHU , Sem 4 ,id 2011084 "],
+  ["mr naveen garg is the hod of second year"],
+  ["mr amit kumar mishra is the hod of first year"],
+  ["our honourable chairman sir dr. kamal ghansyala"],
+  ["that's my job sir "],
+  ["their are 3 section a,b,c in 2year odd sem"],
+  ["their are 70 student in section a"],
+  ["their are 68 student in section b"],
+  ["their are 75 student in section b"],
+  ["classcordinator of section a is rakesh patra sir"],
+  ["classcordinator of section b is dr vikrant sir"],
+  ["classcordinator of section c is rahul chauhan sir"],
+  ["12 companies visted till now"],
+  ["Divyansh Pant"],
+  ["sahil negi is my friend"],
+]
+
+
+const alternative = [
+  "next question",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+
+
